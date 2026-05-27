09ae97dbedd1212dc091aef0ee04c2536e9c1071
jeongtaecoding-jpg <jeongtae.coding@gmail.com>
2026-05-27 17:36:52 +0900

    260527 [develop] JwtProvider,  로그인 작업중

--- src/main/java/com/msa4meerkatgram/global/errors/GlobalExceptionHandler.java
+++ src/main/java/com/msa4meerkatgram/global/errors/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.msa4meerkatgram.global.errors;
 
+import com.msa4meerkatgram.global.errors.custom.NotRegisteredException;
 import com.msa4meerkatgram.global.responses.GlobalRes;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,17 @@ import java.util.List;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(NotRegisteredException.class)
+    public ResponseEntity<GlobalRes<String>> NotRegisteredHandle(NotRegisteredException e) {
+        return ResponseEntity.status(400).body(
+                GlobalRes.<String>builder()
+                        .code("E01")
+                        .message("로그인 에러")
+                        .data(e.getMessage())
+                        .build()
+        );
+    }
+
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<GlobalRes<String>> methodArgumentTypeMismatchHandle(MethodArgumentTypeMismatchException e) {
         return ResponseEntity.status(400).body(
