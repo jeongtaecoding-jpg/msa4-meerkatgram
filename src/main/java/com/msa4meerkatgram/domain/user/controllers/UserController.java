32ae368df4581cc2ac6f59390071f8717d671bc8
jeongtaecoding-jpg <jeongtae.coding@gmail.com>
2026-05-28 18:07:50 +0900

    260528 [develop] 쿠키 매니저 완료, 로그인 작업중

--- src/main/java/com/msa4meerkatgram/domain/user/controllers/UserController.java
+++ src/main/java/com/msa4meerkatgram/domain/user/controllers/UserController.java
@@ -1,11 +1,7 @@
 package com.msa4meerkatgram.domain.user.controllers;
 
-import com.msa4meerkatgram.domain.auth.responses.AuthRes;
 import com.msa4meerkatgram.domain.user.services.UserService;
-import com.msa4meerkatgram.global.responses.GlobalRes;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,24 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/test")
-    public ResponseEntity<GlobalRes<AuthRes>> test() {
-        // AuthRes result = userService.test();
-        //
-        // GlobalRes<AuthRes> globalRes = GlobalRes.<AuthRes>builder()
-        //         .code("00")
-        //         .message("정상 처리")
-        //         .data(result)
-        //         .build();
-        //
-        // return ResponseEntity.status(200).body(globalRes);
-
-        return ResponseEntity.status(200).body(
-            GlobalRes.<AuthRes>builder()
-                    .code("00")
-                    .message("정상 처리")
-                    .data(userService.test())
-                    .build()
-        );
-    }
+    // @GetMapping("/test")
+    // public ResponseEntity<GlobalRes<AuthRes>> test() {
+    //     // AuthRes result = userService.test();
+    //     //
+    //     // GlobalRes<AuthRes> globalRes = GlobalRes.<AuthRes>builder()
+    //     //         .code("00")
+    //     //         .message("정상 처리")
+    //     //         .data(result)
+    //     //         .build();
+    //     //
+    //     // return ResponseEntity.status(200).body(globalRes);
+    //
+    //      // 윗쪽의 코드를 아래로 대체할 수 있다.
+    //     return ResponseEntity.status(200).body(
+    //         GlobalRes.<AuthRes>builder()
+    //                 .code("00")
+    //                 .message("정상 처리")
+    //                 .data(userService.test())
+    //                 .build()
+    //     );
+    // }
 }
