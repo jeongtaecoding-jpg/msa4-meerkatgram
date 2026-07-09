7b8fe2b26736c365afa70ad43edd0edf6b9b8054
jeongtaecoding-jpg <jeongtae.coding@gmail.com>
2026-07-09 18:09:13 +0900

    [260709] add Springdoc OpenApi

--- src/main/java/com/msa4meerkatgram/global/responses/GlobalRes.java
+++ src/main/java/com/msa4meerkatgram/global/responses/GlobalRes.java
@@ -1,12 +1,14 @@
 package com.msa4meerkatgram.global.responses;
 
-import lombok.Builder;
-import lombok.Getter;
+public record GlobalRes<T>(
+        String code
+        , String message
+        , T data
+) {
+    public static <T> GlobalRes<T> from(String code, String message, T data) {
+        return new GlobalRes<T>(code, message, data);
 
-@Getter
-@Builder
-public class GlobalRes<T> {
-    private String code;
-    private String message;
-    private T data;
+        // Static 메소드는 GlobalRes가 인스턴스되서 저장되는 영역과 별개로 저장된다.(정보가 다르다)
+        // 따라서 제네릭은 별도로 저장해야 한다.
+    }
 }
